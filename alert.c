d8099f2273b06042150abf989f1ab17cd7660865
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2021-10-24 23:11:35 +0530

    Update alert.c

--- alert.c
+++ alert.c
@@ -2,6 +2,9 @@
 #include "stats.h"
 #include "alert.h"
 
+int emailAlertCallCount = 0;
+int ledAlertCallCount = 0;
+
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats){
   if(computedStats.max > maxThreshold){
       emailAlerter();
